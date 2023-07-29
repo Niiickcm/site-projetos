562f00f0df25cc2e43bc895f59632205c81f6cca
Niiickcm <thiagomiguel.05@hotmail.com>
2023-07-29 11:41:05 -0300

    fix netflix

--- src/app/netflix/layout.tsx
+++ src/app/netflix/layout.tsx
@@ -7,18 +7,6 @@ const inter = Inter({ subsets: ["latin"] });
 import Header from "../../components/nextflix/Header";
 import Footer from "../../components/nextflix/Footer";
 
-export const metadata = {
-  title: {
-    default: "Thiago Miguel",
-    template: "%s | Thiago Miguel",
-  },
-  robots: {
-    index: true,
-    follow: true,
-  },
-  description: "Thiago Miguel.",
-};
-
 export default function RootLayout({
   children,
 }: {
